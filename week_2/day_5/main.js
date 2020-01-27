0252d7e3e882fc008f1c11595d897f3f86884c51
Rachel Jakob <racheljakob@Rachels-MacBook-Air-2.local>
2020-01-27 12:57:02 +0200

    mini project

--- week_2/day_5/main.js
+++ week_2/day_5/main.js
@@ -1,6 +1,31 @@
-let word = "hello";
+let game = alert ("Welcome to hangman!");
+const word = "abstract";
+const chances = 10;
+let wordArr = word.split('');
+let userArr = [];
 
-let wordCapitalized = word.charAt(0).toUpperCase() + word.slice(1);
-console.log(wordCapitalized);
-
-
+ for (var i = 0; i < wordArr.length; i++) {
+   if(i == 0 || i == wordArr.length-1){
+    userArr.push(wordArr[i]);
+  }
+  else {
+    userArr.push('*');
+   }
+  }
+  alert(userArr.join(''));
+ 
+ for (let i = 0; i < 10; i++){
+  let guess = prompt("Guess a letter to figure out the word");
+   if (guess.length<=1){
+    for (let j = 0; j < wordArr.length; j++) {
+      if (wordArr[j].toLowerCase() == guess.toLowerCase()) {
+        userArr[j] = wordArr[j];
+      }
+    }
+    alert(userArr.join(''));
+    if(word == userArr.join('')){
+    alert("Good job! The answer " + word + " was correct!");
+    break;
+    }
+  }
+}
