7e5e0735fe95e89255d473db45d0adb70bbb4a5e
Rachel Jakob <racheljakob@Rachels-MacBook-Air-2.local>
2020-02-13 11:46:10 +0200

    star wars exercise with import

--- week_5/day_4/exerciseFeb12.py
+++ week_5/day_4/exerciseFeb12.py
@@ -15,12 +15,15 @@ class ForceWielder:
     def fight(self, other):
         fighter1 = statistics.harmonic_mean([self.power, self.wisdom])
         fighter2 = statistics.harmonic_mean([other.power, other.wisdom])
-        print(fighter1)
-        print(fighter2)
+        # print(fighter1)
+        # print(fighter2)
         if fighter1 > fighter2:
-            print(f'winner is, {self.name}')
+            print(f'The winner is, {self.name}')
+            return True
         else:
-            print(f'winner is, {other.name}')
+            print(f'The winner is, {other.name}')
+            return False
+
 
     def is_jedi(self):
         pass
@@ -35,15 +38,15 @@ class Jedi(ForceWielder):
             self.lightsaber_color = 'Blue'
         elif self.power == self.wisdom:
             self.lightsaber_color = 'Violet'
-        print(self.lightsaber_color)
+        # print(self.lightsaber_color)
 
         self.wisdom = self.wisdom + 10
-        print(self.wisdom)
+        print(f'Jedi wisdom +10 {self.wisdom}')
 
     def train(self):
         self.wisdom = self.wisdom + random.randint(10,20)
         self.power = self.power + random.randint(5,15)
-        print(self.wisdom, self.power)
+        print(f'Jedi wisdom and power increases {self.wisdom}, {self.power}')
 
     def is_jedi(self):
        return True
@@ -52,33 +55,34 @@ class Sith(ForceWielder):
     def __init__(self, name):
         super().__init__(name)
 
-        self.name = "Darth" + name
-        print(self.name)
+        self.name = "Darth " + name
+        # print(self.name)
 
         self.lightsaber_color = 'red'
-        print(self.lightsaber_color)
+        # print(self.lightsaber_color)
+
         self.power = self.power + 10
-        print(self.power)
+        print(f'Sith power +10 {self.power}')
 
     def train(self):
         self.wisdom = self.wisdom + random.randint(5, 15)
-        self.power = self.power + random.randint(10, 120)
-        print(self.wisdom, self.power)
+        self.power = self.power + random.randint(10, 20)
+        print(f'Sith wisdom and power increases {self.wisdom}, {self.power}')
 
     def is_jedi(self):
         return False
 
-p1 = ForceWielder("Abby")
-p2 = ForceWielder("Rachel")
-p1.fight(p2)
-jedi = Jedi("Anakin")
-
-jedi.train()
-print(jedi.is_jedi())
-
-p3 = Sith(" Mitch")
-p3.train()
-print(p3.is_jedi())
+# p1 = ForceWielder("Abby")
+# p2 = ForceWielder("Rachel")
+# p1.fight(p2)
+# jedi = Jedi("Anakin")
+#
+# jedi.train()
+# print(jedi.is_jedi())
+#
+# p3 = Sith(" Mitch")
+# p3.train()
+# print(p3.is_jedi())
 
 
 
